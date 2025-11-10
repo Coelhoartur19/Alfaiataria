cd94d3f80717993826b0d284bf9d7c265155f19f
brocolis <162514135+Ghlxavier@users.noreply.github.com>
2025-11-10 17:00:37 -0300

    Update loja.sql

--- loja.sql
+++ loja.sql
@@ -269,11 +269,11 @@ DELIMITER ;
 
 -- Views de acesso (cliente e funcionário)
 
-CREATE VIEW v_produtos_cliente AS
+CREATE VIEW vw_produtos_cliente AS
 SELECT IDProduto, Nome, Preco, Estoque
 FROM Produtos;
 
-CREATE VIEW v_produtos_funcionario AS
+CREATE VIEW vw_produtos_funcionario AS
 SELECT IDProduto, Nome, Preco, Estoque
 FROM Produtos;
 
@@ -282,12 +282,12 @@ CREATE USER funcionario@localhost IDENTIFIED BY 'funcionario';
 CREATE USER cliente@localhost IDENTIFIED BY 'cliente';
 
 grant all on loja.* to gerencia@localhost;
-grant select on loja.v_produtos_funcionario TO funcionario@localhost ;
+grant select on loja.vw_produtos_funcionario TO funcionario@localhost ;
 grant select on loja.Clientes TO funcionario@localhost;
 grant select on loja.Funcionarios TO funcionario@localhost;
 grant select , INSERT, UPDATE, DELETE ON loja.Vendas    TO funcionario@localhost;
 grant select , INSERT, UPDATE, DELETE ON loja.ItensVenda TO funcionario@localhost;
-grant select on loja.v_produtos_cliente TO cliente@localhost;
+grant select on loja.vw_produtos_cliente TO cliente@localhost;
 
 insert into grupos_usuarios (NomeGrupo, Descricao)
 values ('Administrador','Acesso administrativo'),
@@ -296,14 +296,14 @@ values ('Administrador','Acesso administrativo'),
 ON DUPLICATE KEY UPDATE Descricao = VALUES(Descricao);
 
 
-CREATE OR REPLACE VIEW v_front_produtos AS
+CREATE OR REPLACE VIEW vw_front_produtos AS
 SELECT  p.IDProduto   AS id,
         p.Nome        AS nome,
         p.Categoria   AS categoria,
         p.Preco       AS preco
 FROM Produtos p;
 
-CREATE OR REPLACE VIEW v_front_usuarios AS
+CREATE OR REPLACE VIEW vw_front_usuarios AS
 SELECT  u.IDUsuario   AS id,
         u.Nome        AS nome,
         u.Email       AS email,
@@ -311,12 +311,12 @@ SELECT  u.IDUsuario   AS id,
 FROM usuarios u
 JOIN grupos_usuarios g ON g.IDGrupo = u.IDGrupo;
 
-CREATE OR REPLACE VIEW v_front_grupos AS
+CREATE OR REPLACE VIEW vw_front_grupos AS
 SELECT g.IDGrupo AS id, g.NomeGrupo AS nome, g.Descricao AS descricao
 FROM grupos_usuarios g;
 
-grant select on loja.v_front_produtos to funcionario@localhost, cliente@localhost;
-grant select on loja.v_front_usuarios to gerencia@localhost;   -- geralmente só gerência enxerga usuários
-grant select on loja.v_front_grupos   to gerencia@localhost, funcionario@localhost, cliente@localhost;
+grant select on loja.vw_front_produtos to funcionario@localhost, cliente@localhost;
+grant select on loja.vw_front_usuarios to gerencia@localhost;   -- geralmente só gerência enxerga usuários
+grant select on loja.vw_front_grupos   to gerencia@localhost, funcionario@localhost, cliente@localhost;
 
 flush privileges;
